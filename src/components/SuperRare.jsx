bfd7a816b82de9aed6023b8af1d645447c030943
levansardalashvili <sardalaalevani@gmail.com>
2022-11-21 12:01:57 +0400

    nft commit

---
+++ src/components/SuperRare.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Card from "./Card";
+import super1 from "../assets/barcelona.png";
+import super2 from "../assets/bayern.png";
+import super3 from "../assets/chelsea.png";
+import super4 from "../assets/liverpool.png";
+
+export default function SuperRare() {
+  const data = [
+    {
+      image: super1,
+      series: "Gloop Series",
+      title: "Barcelona",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+    {
+      image: super2,
+      series: "Gloop Series",
+      title: "Bayern",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+    {
+      image: super3,
+      series: "Gloop Series",
+      title: "Chelsea",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+    {
+      image: super4,
+      series: "Gloop Series",
+      title: "Liverpool",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+  ];
+  return (
+    <div className="super-rare">
+      <div className="title-container">
+        <h2 className="title">LE Super Rare Auction</h2>
+        <p className="description">
+          We have released four limited edition NFT's early which which can be
+          bid on via <a href="#">OpenSea</a>.
+        </p>
+      </div>
+      <div className="cards">
+        {data.map(({ image, series, title, price, tag, time }, index) => (
+          <Card
+            image={image}
+            series={series}
+            title={title}
+            price={price}
+            tag={tag}
+            time={time}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
